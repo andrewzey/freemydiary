5f57e78738e5e0b0b21c58fc69499684fbe32e61
Andrew Zey <andrew@andrewzey.com>
2014-10-12 11:12:13 -0700

    (feat) make response more informative

--- requestHandlers.js
+++ requestHandlers.js
@@ -5,7 +5,7 @@ var requestHandlers = {};
 requestHandlers.userCheck = function(req, res){
   var username = req.body.username;
   mfp.diaryStatusCheck(username, function(status) {
-    res.send(status);
+    res.send('Username: '+ username + ', Diary: ' + status);
   });
 };
 
